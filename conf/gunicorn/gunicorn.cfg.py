aabb4e6b66dc4c245d5d1056a2bb09345aa7eacd
Cuong Nguyen <cuongnguyen23@gmail.com>
2016-08-09 15:01:24 +0700

    update gunicorn port configuration

--- conf/gunicorn/gunicorn.cfg.py
+++ conf/gunicorn/gunicorn.cfg.py
@@ -4,10 +4,12 @@
 Gunicorn configuration file
 """
 
+import os
+
 from multiprocessing import cpu_count
 
-
-bind = "0.0.0.0:5000"
+port = int(os.getenv('PORT', 5000))
+bind = "0.0.0.0:{}".format(port)
 workers = cpu_count()
 accesslog = '-'
 reload = True
