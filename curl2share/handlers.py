eb321f1f10e5937b9236126a8ce4909990ab6366
Cuong Nguyen <cuongnguyen23@gmail.com>
2016-08-21 23:07:45 +0700

    Add redis as caching layer

--- curl2share/handlers.py
+++ curl2share/handlers.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 import os
 import logging
 
-from flask import Flask, request, make_response, \
-    send_from_directory, abort, url_for, render_template
+from flask import Flask, request, make_response, abort, url_for, render_template
 from werkzeug.utils import secure_filename
 
-from config import MAX_FILE_SIZE, STORAGE
+import config
 from curl2share import utils
 
-if STORAGE == 'S3':
-    from curl2share.storage import S3 as Storage
-elif STORAGE == 'FILESYSTEM':
-    from config import UPLOAD_DIR
-    from curl2share.storage import FileSystem as Storage
+if config.STORAGE == 'S3':
+    from curl2share.storage import S3, Redis
+    s3 = S3()
+elif config.STORAGE == 'LOCAL':
+    from curl2share.storage import FileSystem
+    fs = FileSystem()
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE * 1024 * 1024
-
-s = Storage()
+app.config['MAX_CONTENT_LENGTH'] = config.MAX_FILE_SIZE * 1024 * 1024
 
 logger = logging.getLogger(__name__)
 
@@ -29,33 +27,42 @@ logger = logging.getLogger(__name__)
 @app.errorhandler(400)
 def bad_request(err):
     ''' HTTP 400 code '''
+    logger.error('Invalid request: {} {}.'.format(request.method, request.path))
     return make_response('Bad Request', 400)
 
 
 @app.errorhandler(404)
 def not_found(err):
     ''' HTTP 404 code '''
+    logger.error('File not found: {}'.format(request.path))
     return make_response('Not Found', 404)
+
+
+@app.errorhandler(411)
+def no_contentlength(err):
+    ''' HTTP 411 code '''
+    return make_response('File Is Empty', 411)
 
 
 @app.errorhandler(405)
 def not_allowed(err):
     ''' HTTP 405 code '''
+    logger.error('Method not allowed: {} {}'.format(request.method, request.path))
     return make_response('Method Not Allowed', 405)
 
 
 @app.errorhandler(413)
 def file_too_large(err):
     ''' HTTP 413 code '''
-    return make_response('File too large. Limit {}MB'.format(MAX_FILE_SIZE),
-                         413)
+    size = request.content_length / 1024 / 1024
+    logger.error('Request {} {} file too large {}MB.'.format(request.path, request.method, size))
+    return make_response('File too large. Limit {}MB'.format(config.MAX_FILE_SIZE), 413)
 
 
 @app.errorhandler(500)
 def internal_error(err):
     ''' HTTP 500 code '''
-    return make_response('Something went wrong. Sorry for this inconvenience',
-                         500)
+    return make_response('Something went wrong. Sorry for this inconvenience', 500)
 
 
 @app.route('/', methods=['GET'])
@@ -78,7 +85,7 @@ def upload(file_name):
         filesize = req.tell()
         req.seek(0)
         utils.validate_filesize(filesize)
-        # Handle in case no file_name input.
+        # Handle in case no file_name in request.
         # Eg: curl -X POST -F file=@file server
         if not file_name:
             fname = secure_filename(req.filename)
@@ -86,7 +93,7 @@ def upload(file_name):
             fname = secure_filename(file_name)
     elif not ct and file_name:
         # Request sent file by stream must have file_name
-        # Eg: curl -X POST|PUT --upload-file myfile
+        # Eg: curl -X POST|PUT --upload-file myfile server
         req = request.stream
         filesize = request.content_length
         utils.validate_filesize(filesize)
@@ -94,20 +101,19 @@ def upload(file_name):
     else:
         logger.error('Invalid request header: \n{}'.format(request.headers))
         abort(400)
+
     dest = '/'.join([sdir, fname])
-    if STORAGE == 'FILESYSTEM':
-        try:
-            os.mkdir(os.path.join(UPLOAD_DIR, sdir))
-        except:
-            logger.error('Failed to create {}'.format(sdir))
-            abort(500)
-        s.write(os.path.join(UPLOAD_DIR, dest), req)
-    if STORAGE == 'S3':
-        partsize = 1024*1024*5
+
+    if config.STORAGE == 'LOCAL':
+        fs.write(dest, req)
+
+    if config.STORAGE == 'S3':
+        partsize = 1024 * 1024 * 5
         if filesize >= partsize:
-            s.upload_multipart(dest, req)
+            s3.upload_multipart(dest, req)
         else:
-            s.upload(dest, req)
+            s3.upload(dest, req)
+
     url = url_for("preview", path=dest, _external=True)
 
     return url, 201
@@ -118,17 +124,15 @@ def download(path):
     '''
     Return file.
     This method should be used in development only.
+    In production, you should consider using nginx for this.
     '''
     filename = secure_filename(os.path.basename(path))
-    if STORAGE == 'S3':
-        body = s.get(path)
+    if config.STORAGE == 'S3':
+        body = s3.get(path)
         resp = make_response(body)
-    if STORAGE == 'FILESYSTEM':
-        try:
-            resp = make_response(send_from_directory(UPLOAD_DIR, path))
-        except:
-            logger.error('Unable to download {}'.format(path), exc_info=True)
-            abort(500)
+
+    if config.STORAGE == 'LOCAL':
+        resp = fs.get(path)
 
     resp.headers['Content-Disposition'] = \
         'attachment; filename="{}"'.format(filename)
@@ -139,25 +143,35 @@ def download(path):
 @app.route('/<path:path>', methods=['GET'])
 def preview(path):
     ''' Render a preview page based on file information '''
-    logger.info('Render preview page for {}'.format(path))
+    logger.info('Rendering preview page for {}'.format(path))
     dl_url = url_for('download', path=path, _external=True)
 
-    if STORAGE == 'S3':
-        info = s.info(path)
-        filename = os.path.basename(path)
+    if config.STORAGE == 'S3':
+        if config.REDIS:
+            # try to get file info from redis first
+            # if no info available, then get info from S3 and
+            # insert back to redis for future use
+            redis = Redis()
+            info = redis.get(path)
+            if not info:
+                info = s3.info(path)
+                redis.set(path, info)
+        else:
+            info = s3.info(path)
         filesize = info['content_length']
         filetype = info['content_type']
-    if STORAGE == 'FILESYSTEM':
-        dst = os.path.join(UPLOAD_DIR, path)
+
+    if config.STORAGE == 'LOCAL':
+        dst = os.path.join(config.UPLOAD_DIR, path)
         if not os.path.isfile(dst):
             abort(404)
-        filename = os.path.basename(dst)
+
         filesize = os.path.getsize(dst)
-        filetype = s.mime(dst)
+        filetype = fs.mime(dst)
 
     return render_template('preview.html',
-                           title=filename,
-                           file_name=filename,
+                           title=os.path.basename(path),
+                           file_name=os.path.basename(path),
                            file_size=filesize,
                            file_type=filetype,
                            url=dl_url
